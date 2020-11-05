00071bc36be4e695fe240f0832c650da9734f370
Another-Person <manjoey7@gmail.com>
2020-11-04 19:08:58 -0600

    Created basic account class

---
+++ Account.h
@@ -0,0 +1,27 @@
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+
+class Account
+{
+public:
+	enum Status
+	{
+		Closed,
+		Open
+	};
+
+	Account(int number, Status acctStatus, double startBalance): acctNumber(number), status(acctStatus), balance(startBalance) {}
+
+	void closeAccount() { status = Status::Closed; }
+	void openAccount() { status = Status::Open; }
+	virtual void show() = 0;
+	
+protected:
+	int acctNumber;
+	Status status;
+	double balance;
+	
+};
+
+#endif
+
